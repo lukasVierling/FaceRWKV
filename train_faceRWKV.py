0701ac0cfc97bb352b35bd0fb4b5e2a7c815dd1f
Lukas Vierling <lukas.vierling@rwth-aachen.de>
2023-11-20 17:31:20 +0800

    main fix and to device for gpu trianing

--- train_faceRWKV.py
+++ train_faceRWKV.py
@@ -12,54 +12,70 @@ import tensorboardX
 from models.FaceRWKV import FaceRWKV, RWKVConfig
 from Dataset import CAERSRDataset
 
-# get the data set
-data_dir = 'data/CAER-S/train'
-dataset = CAERSRDataset(data_dir)
-trainloader = data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
+def main():
+    # get the data set
+    data_dir = 'data/CAER-S/train'
+    dataset = CAERSRDataset(data_dir)
+    trainloader = data.DataLoader(dataset, batch_size=4, shuffle=True, num_workers=2)
 
-# get the model
-config = RWKVConfig()
-config.num_classes = dataset.get_classes()
-model = FaceRWKV(config)
-
-# get the loss function
-criterion = nn.CrossEntropyLoss()
-
-# get the optimizer
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-
-#tensorboard setup
-writer = tensorboardX.SummaryWriter()
-
-# train the model
-num_epochs = 30
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999: # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss/2000))
-            running_loss = 0.0
-            #tensorboard logging
-            writer.add_scalar('loss', loss.item(), epoch*len(trainloader)+i)
-            writer.add_scalar('accuracy', 0.0, epoch*len(trainloader)+i)
-    if epoch % 5 == 4:
-        torch.save(model.state_dict(), 'models/CAER-S/epoch' + str(epoch+1) + '.pth')
+    # get the model
+    config = RWKVConfig()
+    config.num_classes = dataset.get_classes()
+    model = FaceRWKV(config)
+    CUDA = False
+    if CUDA:
+        device = torch.device('cuda')
+    else:
+        device = torch.device('cpu')
+    model.to(device)
 
 
-print('Finished Training')
-#save last model
-torch.save(model.state_dict(), 'models/CAER-S/epoch' + str(num_epochs) + '.pth')
+
+    # get the loss function
+    criterion = nn.CrossEntropyLoss()
+    criterion = criterion.to(device)
+
+    # get the optimizer
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    #tensorboard setup
+    writer = tensorboardX.SummaryWriter()
+
+    # train the model
+    num_epochs = 30
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999: # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' % (epoch+1, i+1, running_loss/2000))
+                running_loss = 0.0
+                #tensorboard logging
+                writer.add_scalar('loss', loss.item(), epoch*len(trainloader)+i)
+                writer.add_scalar('accuracy', 0.0, epoch*len(trainloader)+i)
+        if epoch % 5 == 4:
+            torch.save(model.state_dict(), 'models/CAER-S/epoch' + str(epoch+1) + '.pth')
+
+
+    print('Finished Training')
+    #save last model
+    torch.save(model.state_dict(), 'models/CAER-S/epoch' + str(num_epochs) + '.pth')
+
+if __name__ == "main":
+    main()
