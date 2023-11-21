326449a82f48c16aeb64b75ae6611b70e033371a
Lukas Vierling <lukas.vierling@rwth-aachen.de>
2023-11-21 16:23:47 +0800

    cnn for comparison

---
+++ models/CNNmodel.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+
+class CNNClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super(CNNClassifier, self).__init__()
+        
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(32),
+            
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(64),
+            
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(128),
+            
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(256),
+            
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.BatchNorm2d(512)
+        )
+        
+        self.fc_layers = nn.Sequential(
+            nn.Linear(512 * 25 * 37, 512),
+            nn.ReLU(),
+            nn.BatchNorm1d(512),
+            
+            nn.Linear(512, num_classes)
+        )
+        
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_layers(x)
+        return x
