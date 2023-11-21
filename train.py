d69db09b82438852346a9ea7ac308ce81c2ccb73
Lukas Vierling <lukas.vierling@rwth-aachen.de>
2023-11-21 15:55:19 +0800

    added checkpoint saving

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ import torchvision.transforms as transforms
 import numpy as np
 import torch.utils.data
 from tqdm import tqdm
+import socket
+import time
 
 import tensorboardX
 
@@ -88,11 +90,11 @@ def main():
         writer.add_scalar('val_acc', val_acc, epoch)
         # save model every 5 epochs
         if epoch % 5 == 4:
-            torch.save(model.state_dict(), 'models/CAER-S/epoch' + str(epoch+1) + '.pth')
+            torch.save(model.state_dict(), f'checkpoints/run_{socket.gethostname()}_{time.time()}/epoch' + str(epoch+1) + '.pth')
 
     print('Finished Training')
     # save last model
-    torch.save(model.state_dict(), 'models/CAER-S/epoch' + str(num_epochs) + '.pth')
+    torch.save(model.state_dict(), f'checkpoints/run_{socket.gethostname()}_{time.time()}/epoch' + str(num_epochs) + '.pth')
 
 def validate(model, valloader, device):
     correct = 0
