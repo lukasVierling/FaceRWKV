39d789f9a707d02c9e8159a5a91e9f42fc70f173
Lukas Vierling <lukas.vierling@rwth-aachen.de>
2023-11-22 12:45:21 +0800

    Added sequencing models

---
+++ models/Preprocessing.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class LinearSequencing(nn.Module):
+    def __init__(self, patch_size, embed_dim):
+        super(LinearSequencing, self).__init__()
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+
+        self.linear = nn.Linear(patch_size**2 * 3, embed_dim)
+        self.layer_norm = nn.LayerNorm(embed_dim)
+
+    def forward(self, x):
+        x = self.image_to_patches(x)
+        # Flatten for linear layer
+        x = x.flatten(2)
+        x = self.linear_projection(x)
+        x = self.layer_norm(x)
+        return x
+    
+    def image_to_patches(self, input_image):
+        """
+        Convert input images into patches of defined size.
+
+        Args:
+        - input_image: Input image tensor, shape (batch_size, C, H, W).
+        - patch_size: Size of patches to extract.
+
+        Returns:
+        - Patches reshaped, shape (batch_size, C, n_patches, patch_size, patch_size).
+
+        """
+        N, C, H, W = input_image.shape
+        P = self.patch_size
+        assert H % P == 0 and W % P == 0, "Image dimensions should be divisible by patch size."
+
+        patches = input_image.unfold(2, P, P).unfold(3, P, P)
+        patches = patches.contiguous().view(N, C, -1, P, P)
+        patches = patches.permute(0, 2, 1, 3, 4).contiguous()
+
+        return patches
+    
+class CNNSequencing(nn.Module):
+    def __init__(self, patch_size, embed_dim): #patch_size basically irrelevant here
+        super(CNNSequencing, self).__init__()
+        # load a pretrained resnet and up to stage 3
+        self.resnet = models.resnet50(pretrained=True)
+        # Remove the layers after stage 4
+        self.resnet = torch.nn.Sequential(*list(self.resnet.children())[:6])
+        self.lin = torch.nn.Linear(2048, embed_dim)
+        self.layer_norm = nn.LayerNorm(embed_dim)
+
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+
+    def forward(self, x):
+        x = self.resnet(x)
+        #output shape is: [batch_size, 2048, 12, 18]
+        print(x.shape)
+        N, C, H, W = x.size()
+        x = x.view(N, C, -1).permute(0, 2, 1)
+        x = self.lin(x)
+        x = self.layer_norm(x)
+        return x
+
